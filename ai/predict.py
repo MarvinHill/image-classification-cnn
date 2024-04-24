3dcb2d2c3141136edf8966e88024c96ea39fdbcd
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-04-24 10:41:37 +0200

    made prediction work

--- ai/predict.py
+++ ai/predict.py
@@ -1,6 +1,24 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
 
 def main():
-    trained_model = tf.keras.models.load_model('trained_model.keras');
+    trained_model = tf.keras.models.load_model('trained_model.keras')
 
-    trained_model.predict()
-    # Todo - predict some test data
+    data_classes = np.load("classes.npy")
+    print(data_classes)
+    img = Image.open("img_1.png").resize((180, 180))
+    # Convert to 3 shapes
+    img = img.convert('RGB')
+    img = np.array(img)
+
+    result = trained_model.predict(img[None, :, :])
+    max = np.argmax(result)
+    character = data_classes[max]
+    print(character)
+
+
+if __name__ == "__main__":
+    main()
